a975fe4de83ad66db951f6979092e172557e8bcd
Nick Van <nvan@ucdavis.edu>
2020-02-01 13:42:34 -0800

    Implementing Queue: * Create struct for 'queue' * Implement 'queue_create' and its test: 'test_create' * Implement 'queue_destroy' and its test: 'test_destroy' * Implement 'queue_enqueue' and 'queue_dequeue' * Implement 'queue_delete' and its test: 'test_delete'

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,34 +4,104 @@
 
 #include "queue.h"
 
+/* Choosing Linked List over Array due to enqueue() and dequeue() being O(1)
+ as required by the assignment. (Array's enqueue() and dequeue() is Armortized O(1) */
+struct Node {
+    void *data;
+    struct Node* next;
+};
+
 struct queue {
 	/* TODO Phase 1 */
+	struct Node* head;
+	struct Node* back;
 };
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
-}
+    struct queue* newQueue = NULL;
+    newQueue = (struct queue*)malloc(sizeof(struct queue));
+    if(newQueue != NULL) {
+        newQueue->head = NULL;
+        newQueue->back = NULL;
+    } // If: newQueue is malloc-ed successfully
+    else {
+        return NULL;
+    } // Else: newQueue failed to be allocated
+
+    return newQueue;
+} // queue_create() -- Tested
 
 int queue_destroy(queue_t queue)
 {
 	/* TODO Phase 1 */
-}
+	if(queue == NULL || queue->head != NULL) {
+	    return -1;
+	} // If: Queue is NULL, or Queue is not empty
+
+	free(queue);
+	return 0;
+} // queue_destroy()
 
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
-}
+	if(queue == NULL || data == NULL) {
+	    return -1;
+	} // If: Queue or Data is NULL
+
+	if(queue->head == NULL) {
+	    queue->head = (struct Node*)malloc(sizeof(struct Node));
+	    queue->head->data = data;
+	    queue->back = queue->head;
+	} // If: Empty Queue
+	else {
+	    queue->back->next = (struct Node*)malloc(sizeof(struct Node));
+	    queue->back->next->data = data;
+	    queue->back = queue->back->next;
+	} // Else: Existing Queue
+
+	return 0;
+} // queue_enqueue() -- Tested
 
 int queue_dequeue(queue_t queue, void **data)
 {
 	/* TODO Phase 1 */
-}
+	if(queue == NULL || data == NULL || queue->head == NULL) {
+	    return -1;
+	} // If: Queue/Data is NULL, or if Queue is Empty
+
+	*data = queue->head->data;
+	struct Node* temp = queue->head->next;
+	free(queue->head);
+	queue->head = temp;
+
+	return 0;
+} // queue_dequeue() -- Tested
 
 int queue_delete(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
-}
+	if(queue == NULL || data == NULL) {
+        return -1;
+    } // If: Queue is NULL or Data is NULL
+
+	struct Node *findNode = queue->head;
+
+	while(findNode->next != NULL && findNode->next->data != data) {
+        findNode = findNode->next;
+	} // Find the Node which holds the data to delete
+
+	if(findNode->next == NULL) {
+	    return -1;
+	} // If: Data not found TODO: What if last
+	else {
+	    struct Node *deleteNode = findNode->next;
+	    findNode->next = findNode->next->next;
+	    free(deleteNode);
+	} // Else: Data is found
+
+	return 0;
+} // queue_delete -- Tested
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
 {
--- progs/queue_tester.c
+++ progs/queue_tester.c
@@ -23,6 +23,23 @@ void test_create(void)
 	TEST_ASSERT(queue_create() != NULL);
 }
 
+/* Destroy */
+void test_destroy(void) {
+    int data = 3;
+    fprintf(stderr, "*** TEST destroy ***\n");
+
+    // Check: Return -1 if @queue is NULL
+    queue_t newQueue= NULL;
+    TEST_ASSERT(queue_destroy(newQueue) == -1);
+    // Check: Return -1 if @queue is not empty
+    newQueue = queue_create();
+    queue_enqueue(newQueue, &data);
+    TEST_ASSERT(queue_destroy(newQueue) == -1);
+    // Check: Queue is successfully destroyed
+    newQueue = queue_create();
+    TEST_ASSERT(queue_destroy(newQueue) == 0);
+}
+
 /* Enqueue/Dequeue simple */
 void test_queue_simple(void)
 {
@@ -37,10 +54,33 @@ void test_queue_simple(void)
 	TEST_ASSERT(ptr == &data);
 }
 
+/* Delete Queue */
+void test_delete(void) {
+    int dataArray[10] = {1,2,3,4,5,6,7,8,9,10};
+    queue_t newQueue = queue_create();
+
+    for(int i = 0; i < sizeof(dataArray); i++) {
+        queue_enqueue(newQueue, &dataArray[i]);
+    } // Enqueue 1-10
+
+    fprintf(stderr, "*** TEST queue_delete ***\n");
+
+    queue_delete(newQueue, &dataArray[0]); // Delete '1'
+    TEST_ASSERT(queue_delete(newQueue, &dataArray[0]) != 0); // '1' should not be found
+
+    queue_delete(newQueue, &dataArray[4]); // Delete '5'
+    TEST_ASSERT(queue_delete(newQueue, &dataArray[4]) != 0); // '5' should not be found
+
+    queue_delete(newQueue, &dataArray[9]); // Delete '10'
+    TEST_ASSERT(queue_delete(newQueue, &dataArray[9]) != 0); // '10' should not be found
+}
+
 int main(void)
 {
 	test_create();
 	test_queue_simple();
+	test_destroy();
+    test_delete();
 
 	return 0;
 }
