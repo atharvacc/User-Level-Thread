4e4b635945928440d49459c4645336d93a7a6791
atharvacc <anchalke@ucdavis.edu>
2020-02-01 12:35:11 -0800

    Added files

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -5,19 +5,55 @@
 #include "queue.h"
 
 struct queue {
-	/* TODO Phase 1 */
+	int count; // Get element count
+	void *HEAD;
+	void *TAIL; 
 };
 
+
+/*
+ * queue_create - Allocate an empty queue
+ *
+ * Create a new object of type 'struct queue' and return its address.
+ *
+ * Return: Pointer to new empty queue. NULL in case of failure when allocating
+ * the new queue.
+ */
 queue_t queue_create(void)
 {
+	struct queue newQueue = {NULL, NULL,NULL};
+	return &newQueue;
 	/* TODO Phase 1 */
 }
 
+/*
+ * queue_destroy - Deallocate a queue
+ * @queue: Queue to deallocate
+ *
+ * Deallocate the memory associated to the queue object pointed by @queue.
+ *
+ * Return: -1 if @queue is NULL or if @queue is not empty. 0 if @queue was
+ * successfully destroyed.
+ */
 int queue_destroy(queue_t queue)
 {
-	/* TODO Phase 1 */
+	if (queue == NULL || queue->count != 0){
+		return -1;
+	}
+	free(queue);
+	return 0;
 }
 
+/*
+ * queue_enqueue - Enqueue data item
+ * @queue: Queue in which to enqueue item
+ * @data: Address of data item to enqueue
+ *
+ * Enqueue the address contained in @data in the queue @queue.
+ *
+ * Return: -1 if @queue or @data are NULL, or in case of memory allocation error
+ * when enqueing. 0 if @data was successfully enqueued in @queue.
+ */
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
