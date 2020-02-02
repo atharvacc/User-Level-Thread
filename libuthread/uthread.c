3077808a403a983ecb835b2542d4df60df2acfbe
atharvacc <anchalke@ucdavis.edu>
2020-02-01 21:35:59 -0800

    hw2: Done with uthread create and uthread_self

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -11,21 +11,41 @@
 #include "queue.h"
 #include "uthread.h"
 
-/* TODO Phase 2 */
+struct TCB {
+    uthread_t TID;
+    ucontext_t context;
+	void *stack;
+	int state;
+};
+
+struct TCB *new_tcb; // Initialize a TCB
+int cur_TID = 1; // Initialize cur_TID to 1, since parent has tid 0
+
 
 void uthread_yield(void)
 {
 	/* TODO Phase 2 */
+	
 }
 
 uthread_t uthread_self(void)
 {
-	/* TODO Phase 2 */
+	return new_tcb->TID;
 }
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+	new_tcb = malloc(sizeof(struct TCB));
+	new_tcb->TID = cur_TID;
+	new_tcb->stack = uthread_ctx_alloc_stack();
+	ucontext_t *contextPtr = &(new_tcb->context);
+	int retval = uthread_ctx_init(contextPtr, new_tcb->stack, func, arg);
+	if (retval == -1){
+		return -1;
+	}
+
+	cur_TID++;
+	return new_tcb->TID;
 }
 
 void uthread_exit(int retval)
@@ -37,5 +57,6 @@ int uthread_join(uthread_t tid, int *retval)
 {
 	/* TODO Phase 2 */
 	/* TODO Phase 3 */
+	return 0;
 }
 
