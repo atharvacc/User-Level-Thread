23f538b4b6c7d579701df0eb236fcba51a87ec26
anchalke <anchalke@ad3.ucdavis.edu@pc23.cs.ucdavis.edu>
2020-02-03 20:54:45 -0800

    Done with 5.3

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -17,7 +17,8 @@ struct TCB {
 	void *stack;
 	int state; // 0 for READY, 1 RUNNING, 2 for BLOCKED, 3 for ZOMBIE
 	int retVal; // Store return value
-	int isBlocked; // Check if is blocked
+	int isBlocked; // Check if is blocked(NOTUSED)
+	uthread_t tidWait;
 };
 
 struct TCB *new_tcb; // Initialize a TCB
@@ -38,15 +39,18 @@ void uthread_yield(void)
 	cur_tcb->state = 0; // Set currently running state to READY
 	struct TCB* currThread = cur_tcb; // Create a COPY of the currently running thread
 	struct TCB* nextThread;
-	queue_enqueue(READY, (void**)currThread); // Add to ready queue
+	if(currThread->isBlocked != 1){
+		queue_enqueue(READY, (void**)currThread); // Add to ready queue
+	}
+
 	queue_dequeue(READY, (void**)&nextThread); // Get next thread in queue
 	//printf("len of readyQuee now is %d\n", queue_length(READY));
 	cur_tcb = nextThread; //Get the next thread
 	cur_tcb-> state = 1; // Set state to running
 	ucontext_t* curContext = &(currThread->context); // Get current Context
 	ucontext_t* nextContext = &(cur_tcb->context); // Get next context
-	printf("new TID IS %d\n", nextThread->TID);
-	printf("Curr TID IS %d\n", currThread->TID);
+//	printf("new TID IS %d\n", nextThread->TID);
+//	printf("Curr TID IS %d\n", currThread->TID);
 	uthread_ctx_switch(curContext, nextContext) ; //make the next thread start
 	
 }
@@ -63,11 +67,15 @@ int uthread_create(uthread_func_t func, void *arg)
 		READY = queue_create();
 		cur_tcb = malloc(sizeof(struct TCB));
 		cur_tcb->TID = 0;
+		cur_tcb->tidWait = -2;
+		cur_tcb->isBlocked = 0;
 		cur_TID++;
 	}
 	new_tcb = malloc(sizeof(struct TCB));
 	new_tcb->TID = cur_TID;
 	new_tcb->stack = uthread_ctx_alloc_stack();
+	new_tcb->tidWait = -2;
+	new_tcb->isBlocked=0;
 	ucontext_t *contextPtr = &(new_tcb->context);
 	int retval = uthread_ctx_init(contextPtr, new_tcb->stack, func, arg);
 	if (retval == -1){
@@ -79,22 +87,50 @@ int uthread_create(uthread_func_t func, void *arg)
 	return new_tcb->TID;
 }
 
+static int find_item(void *data, void *arg)
+{
+    struct TCB  *a = (struct TCB*)data;
+    int match = (int)(long)arg;
+    printf("this has TID: %d \n", a->TID);
+    printf("match is: %d \n", match);
+    printf("joinTID is %d \n", a ->tidWait);
+    if (a->tidWait == match){
+	printf(" MATCHED \n");
+        return 1;
+    }
+
+    return 0;
+}
+
 void uthread_exit(int retval)
 {
 	if (ZOMBIE == NULL){ // create zombie if we're in the first state
 		ZOMBIE = queue_create();
 	}
-	printf("IN ZOMBIEEEE \n");
+	
 	cur_tcb ->retVal = retval; // assign retval to that tcb
 	cur_tcb -> state = 4;
 	queue_enqueue(ZOMBIE, cur_tcb); // add to zombie que
-	struct TCB* nextThread;
-	if (queue_length(READY)!=0){
-		queue_dequeue(READY, &nextThread);
-		struct TCB *curThread = cur_TCB;
-		cur_TCB = nextThread;
-		uthread_ctx_switch(&(curThread->context), &(cur_TCB->context));
+
+	struct TCB *curThread = cur_tcb;// create a copy of the current running thread
+	
+	//If blocking something then unblock that and add to READY
+	void *blockedThread;
+	
+	if (queue_iterate(BLOCKED, find_item, (void*)(long)curThread->TID, &blockedThread) == 0){
+	printf("UNBLOCKING THREAD \n");	
+	struct TCB *threadBlocked = (struct TCB*)blockedThread ;
+	threadBlocked->tidWait = -2;
+	queue_delete(BLOCKED, &threadBlocked);
+	queue_enqueue(READY, threadBlocked); 
 	}
+	printf("cur tcb has TID: %d \n", cur_tcb->TID);
+	struct TCB* nextThread;
+	queue_dequeue(READY, &nextThread);
+	cur_tcb = nextThread;
+	// start next thread from READY
+	uthread_ctx_switch(&(curThread->context), &(cur_tcb->context));
+	
 	
 }
 
@@ -103,7 +139,15 @@ int uthread_join(uthread_t tid, int *retval)
 	if (BLOCKED == NULL){ // Create BLOCKED if not created yet
 		BLOCKED = queue_create();
 	}
-	
+	cur_tcb ->tidWait = tid;
+	cur_tcb -> isBlocked = 1;
+	queue_enqueue(BLOCKED, cur_tcb);
+	//printf("cur_tcb has tid: %d \n", cur_tcb->TID);
+	//printf("length of ready queue is: %d \n", queue_length(READY));
+	while(queue_length(READY) != 0){
+		printf("in while \n");
+		uthread_yield();
+	}	
 	return 0;
 }
 
@@ -117,7 +161,6 @@ int main(void)
 {
 	uthread_t tid;
 	tid = uthread_create(hello, NULL);
-	printf("TID IS %d\n", tid);
 	
 	uthread_join(tid, NULL);
 	printf("Done\n");
