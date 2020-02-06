5f7c50299c4d0caa844ede105295a0e8a16802f8
Nick Van <nvan@ucdavis.edu>
2020-02-06 11:55:10 -0800

    Clean up 'preempt.c', change timer to alarm at 100 HZ correctly

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -19,24 +19,19 @@ struct sigaction sa;
 struct itimerval timer;
 sigset_t set ; // Set for blocking signals
 
-//setup singal handler
 void signal_handler(){
-	//printf("inside handler \n");
 	uthread_yield();
 }
 
 void preempt_disable(void)
 {
-	//printf("Disabling preempt \n");
-	sigemptyset(&set); // Set set to empty set 
+	sigemptyset(&set); // Set set to empty set
 	sigaddset(&set, SIGVTALRM); // add SIGVT TO ALARM
 	sigprocmask(SIG_BLOCK, &set, NULL);
-	/* TODO Phase 4 */
 }
 
 void preempt_enable(void)
 {
-	//printf("Enabling preempt \n");
 	sigemptyset(&set);
 	sigaddset(&set, SIGVTALRM);
 	sigprocmask(SIG_UNBLOCK, &set, NULL);
@@ -44,18 +39,12 @@ void preempt_enable(void)
 
 void preempt_start(void)
 {
-	//printf("Starting Preempt \n");
-	memset (&sa, 0, sizeof (sa));
+	memset(&sa, 0, sizeof (sa));
  	sa.sa_handler = &signal_handler;
  	sigaction (SIGVTALRM, &sa, NULL);
-	 timer.it_value.tv_sec = 0;
-	timer.it_value.tv_usec = HZ*100;
-	/* ... and every 250 msec after that. */
+ 	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = HZ/10;
 	timer.it_interval.tv_sec = 0;
-	timer.it_interval.tv_usec = HZ*100;
-	/* Start a virtual timer. It counts down whenever this process is
-	executing. */
+	timer.it_interval.tv_usec = HZ/10;
 	setitimer (ITIMER_VIRTUAL, &timer, NULL);
-	
-
 } 
